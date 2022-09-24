c3d50a2e0a3ec6adb8dc7e17ef171cc78aead752
Samar-Ali666 <sammar.ali2439@gmail.com>
2022-09-24 14:56:09 +0500

    Task 1 completed

---
+++ main.py
@@ -0,0 +1,75 @@
+bookings = []
+
+maxParkingSpace = 20
+continueProgram = True
+
+
+def getRequestedDay():
+    requestedDay = int(input('Enter the day between (1 - 14): \n'))
+
+    while requestedDay < 0 or requestedDay > 14:
+        print('Invalid Day! Kindly enter valid Day')
+        requestedDay = int(input('Enter the day between (1 - 14): \n'))
+
+    return requestedDay
+
+
+def getCarLicence():
+    carLicence = input('Enter the licence number: \n')
+
+    while not carLicence.isalnum():
+        print('Invalid Licence! Special character are not allowed')
+        carLicence = input('Enter the licence number: \n')
+
+    return carLicence
+
+
+def getVisitorName():
+    visitorName = input('Enter the visitor name: \n')
+
+    while not all(name.isalpha() or name.isspace() for name in visitorName):
+        print('Invalid Name! Name should contain only alphabets')
+        visitorName = input('Enter the visitor name: \n')
+
+    return visitorName
+
+
+def checkSpaceAvailability(requestedDay):
+    spaces_occupied_at_requestedDay = 0
+
+    for booking in bookings:
+        if booking["requestedDay"] == requestedDay:
+            spaces_occupied_at_requestedDay += 1
+
+    if spaces_occupied_at_requestedDay >= maxParkingSpace:
+        return -1
+
+    return spaces_occupied_at_requestedDay
+
+
+def createBooking(visitorName, licence, requestedDay):
+    newBooking = {}
+    newBooking['visitorName'] = visitorName
+    newBooking['licence'] = licence
+    newBooking['requestedDay'] = requestedDay
+
+    bookings.append(newBooking)
+
+
+while continueProgram:
+    requestedDay = getRequestedDay()
+    parkingSpace = checkSpaceAvailability(requestedDay)
+
+    if parkingSpace != -1:
+        visitorName = getVisitorName()
+        carLicence = getCarLicence()
+
+        createBooking(visitorName, carLicence, requestedDay)
+
+        print('Parking has been reserved! \nDay ' + str(requestedDay) +
+              '\nParking number: ' + str(parkingSpace + 1))
+
+        continueProgram = input('Continue (yes/no): \n')
+        continueProgram = False if continueProgram == 'no' else continueProgram
+    else:
+        print('No Spaces available at requeted Day! Select Another day \n')
